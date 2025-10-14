1c81c3e998332a80432fc1eda7fd8ca303393973
ivanmc21 <ivanmcuello10@gmail.com>
2025-10-14 09:59:09 -0600

    fix:adjusting the size of titles on service pages

--- src/components/servicios/SectionOne.tsx
+++ src/components/servicios/SectionOne.tsx
@@ -7,27 +7,25 @@ type PresentationType = {
 const SectionOne = ({ title, description, image }: PresentationType) => {
     return (
         <section className="mt-15">
-            <article className="bg-services">
-                <div className="max-w-7xl mx-auto px-10 py-15 grid grid-cols-1 gap-10 lg:gap-0 lg:grid-cols-2 place-items-center">
-                    <div className="space-y-8">
-                        <h2 className="text-3xl md:text-5xl text-title uppercase font-extrabold">{title}</h2>
-                        <p className="text-base md:text-lg text-white">{description}</p>
-                        <button className="text-white text-sm md:text-base border border-white px-4 py-2 rounded-full uppercase hover:bg-white/10 transition-colors cursor-pointer hover:scale-110 hover:transition-transform hover:duration-300">
-                            Quiero conocer
-                        </button>
-                    </div>
-                    <div className="flex justify-center items-center">
-                        <img
-                            src={image}
-                            alt={title}
-                            className="md:w-[380px] md:h-[280px] border-4 border-cafeBosc rounded-2xl object-cover"
-                        />
-
-                    </div>
-                </div>
-
-            </article>
-        </section>
+    <article className="bg-services">
+        <div className=" max-w-7xl mx-auto px-4 sm:px-6 lg:px-15 py-15 flex flex-col lg:flex-row items-center justify-between gap-12 lg:gap-16">
+            <div className="space-y-8  flex-1 max-w-3xl">
+                <h2 className="text-3xl md:text-[2.6rem] text-title uppercase font-extrabold">{title}</h2>
+                <p className="text-base md:text-lg text-white">{description}</p>
+                <button className="text-white text-sm md:text-base border border-white px-4 py-2 rounded-full uppercase hover:bg-white/10 transition-colors cursor-pointer hover:scale-110 hover:transition-transform hover:duration-300">
+                    Quiero conocer
+                </button>
+            </div>
+            <div className=" flex justify-center items-center">
+                <img
+                    src={image}
+                    alt={title}
+                    className="w-[280px] sm:w-[340px] md:w-[380px] md:h-[280px] border-4 border-cafeBosc rounded-2xl object-cover"
+                />
+            </div>
+        </div>
+    </article>
+</section>
     )
 }
 
