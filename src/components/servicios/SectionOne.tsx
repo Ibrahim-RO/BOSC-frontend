7f83d3846d341b4ecca820bb49cc8a30e67e1814
ibrahim <ibra.rodriguez.olaya@gmail.com>
2025-10-15 13:14:42 -0600

    Fix: Redirect contact

--- src/components/servicios/SectionOne.tsx
+++ src/components/servicios/SectionOne.tsx
@@ -14,9 +14,11 @@ const SectionOne = ({ title, description, image }: PresentationType) => {
             <div className="space-y-8  flex-1 max-w-3xl">
                 <h2 className="text-3xl md:text-[2.6rem] text-title uppercase font-extrabold">{title}</h2>
                 <p className="text-base md:text-lg text-white">{description}</p>
-                <button className="text-white text-sm md:text-base border border-white px-4 py-2 rounded-full uppercase hover:bg-white/10 transition-colors cursor-pointer hover:scale-110 hover:transition-transform hover:duration-300">
+                <Link
+                    to={'/#contact'}
+                    className="text-white text-sm md:text-base border border-white px-4 py-2 rounded-full uppercase hover:bg-white/10 transition-colors cursor-pointer hover:scale-110 hover:transition-transform hover:duration-300">
                     Quiero conocer
-                </button>
+                </Link>
             </div>
             <div className=" flex justify-center items-center">
                 <img
